632a09542f8ed6a3ec200006210d492cebf56a3b
saeed-sufi <sufi.geo@gmail.com>
2024-01-06 16:11:30 +0330

    setup a basic fetchdata function

--- 20.design_patterns/index.js
+++ 20.design_patterns/index.js
@@ -1,1 +1,15 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: '392d6eea',
+      s: searchTerm
+    }
+  })
+
+  console.log(response.data)
+}
+
+const input = document.querySelector("#input")
+input.addEventListener("input", (event) => {
+  fetchData(event.target.value)
+})
