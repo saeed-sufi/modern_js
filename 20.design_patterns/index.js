45b4978b0605200d84ad27702642e0afcdf57e80
saeed-sufi <sufi.geo@gmail.com>
2024-01-06 17:37:42 +0330

    Appending the list of searched movies to the html

--- 20.design_patterns/index.js
+++ 20.design_patterns/index.js
@@ -6,13 +6,23 @@ const fetchData = async (searchTerm) => {
     }
   })
 
-  console.log(response.data)
+  return response.data.Search
 }
 
 const input = document.querySelector("#input")
 
-const onInput = (event) => {
-  fetchData(event.target.value)
+const onInput = async (event) => {
+  const movies = await fetchData(event.target.value)
+  
+  for (let movie of movies) {
+    let div = document.createElement('div')
+
+    div.innerHTML = `
+      <img src="${movie.Poster}" />
+      <h1>${movie.Title}</h1>
+    `
+    document.querySelector("#target").appendChild(div)
+  }
 }
 
 input.addEventListener("input", debounce(onInput, 500))
