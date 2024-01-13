499d5213ecb84675e35845db1ee586bc9b575421
saeed.soofi <saeed.soofi@partdp.ai>
2024-01-13 16:25:35 +0330

    Displaying basic details about each selected movie

--- 20.design_patterns/index.js
+++ 20.design_patterns/index.js
@@ -36,22 +36,22 @@ const onInput = async (event) => {
   }
   resultsWrapper.innerHTML = ''
   dropdown.classList.add('is-active')
-
+  
   for (let movie of movies) {
     const option = document.createElement('a')
     const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
-
+    
     option.classList.add('dropdowm-item')
     option.innerHTML = `
-      <img src="${imgSrc}" />
-      ${movie.Title}
+    <img src="${imgSrc}" />
+    ${movie.Title}
     `
     option.addEventListener('click', () => {
       dropdown.classList.remove('is-active');
       input.value = movie.Title;
+      onMovieSelect(movie)
     })
     resultsWrapper.appendChild(option)
-    onMovieSelect(movie)
   }
 }
 
@@ -70,5 +70,24 @@ const onMovieSelect = async movie => {
     }
   })
 
-  console.log(response.data)
- }
+  document.querySelector("#summary").innerHTML = movieTemplate(response.data)
+}
+
+const movieTemplate = movieDetail => {
+  return `
+    <article class = "media">
+      <figure class = "media-left">
+        <p class = "image">
+          <img src="${movieDetail.Poster}" />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class = "content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
+        </div>
+      </div>
+    </article>
+  `
+}
